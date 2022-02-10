75a5e38d26142ae3fa17d0a6889fe8a4aa5666dc
junilpyeon <dreamerjunil@gmail.com>
2022-02-10 13:31:57 +0900

    vitage shop proto

--- src/main/java/com/jpabook/jpashop/domain/Brand.java
+++ src/main/java/com/jpabook/jpashop/domain/Brand.java
@@ -13,35 +13,35 @@ import static javax.persistence.FetchType.LAZY;
 
 @Entity
 @Getter @Setter
-public class Category {
+public class Brand {
 
     @Id @GeneratedValue
-    @Column(name = "category_id")
+    @Column(name = "brand_id")
     private Long id;
 
     private String name;
 
     @ManyToMany
-    @JoinTable(name = "category_item",
-            joinColumns = @JoinColumn(name = "category_id"),
+    @JoinTable(name = "brand_item",
+            joinColumns = @JoinColumn(name = "brand_id"),
             inverseJoinColumns = @JoinColumn(name = "item_id"))
     private List<Item> items = new ArrayList<>();
 
     @ManyToMany
-    @JoinTable(name = "category_item",
-            joinColumns = @JoinColumn(name = "category_id"),
+    @JoinTable(name = "brand_item",
+            joinColumns = @JoinColumn(name = "brand_id"),
             inverseJoinColumns = @JoinColumn(name = "top_id"))
     private List<Top> tops = new ArrayList<>();
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "parent_id")
-    private Category parent;
+    private Brand parent;
 
     @OneToMany(mappedBy = "parent")
-    private List<Category> child = new ArrayList<>();
+    private List<Brand> child = new ArrayList<>();
 
     //==연관관계 메서드==//
-    public void addChildCategory(Category child) {
+    public void addChildCategory(Brand child) {
         this.child.add(child);
         child.setParent(this);
     }
