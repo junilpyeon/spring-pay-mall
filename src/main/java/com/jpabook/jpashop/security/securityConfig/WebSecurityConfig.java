b222393bfbab051c8554f87528b6933ba9f2525d
junilpyeon <junilpyeon@gmail.com>
2022-02-11 19:42:03 +0900

    swagger 적용

--- src/main/java/com/jpabook/jpashop/security/securityConfig/WebSecurityConfig.java
+++ src/main/java/com/jpabook/jpashop/security/securityConfig/WebSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableWebSecurity
